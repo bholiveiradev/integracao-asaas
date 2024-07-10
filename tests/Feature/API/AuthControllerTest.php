3d11c7672346cc75cc65b76c6fb2342792dacf74
Bruno Oliveira <83931779+bholiveiradev@users.noreply.github.com>
2024-07-10 14:22:08 -0300

    test: add fails register a new user with invalid credentials test

--- tests/Feature/API/AuthControllerTest.php
+++ tests/Feature/API/AuthControllerTest.php
@@ -15,3 +15,18 @@ it('should register a new user', function () {
 
     expect($response)->assertStatus(Response::HTTP_CREATED);
 });
+
+it('should fails to register a new user with invalid data', function () {
+    $data = [
+        'name' => '',
+        'email' => 'invalid-email',
+        'password' => '',
+        'password_confirmation' => 'not_matching',
+    ];
+
+    $response = $this->postJson('/api/register', $data);
+
+    expect($response)
+        ->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
+        ->assertJsonValidationErrors(['name', 'email', 'password', 'password_confirmation']);
+});
