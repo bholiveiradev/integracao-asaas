629ece2e84d109802b849e7e1a0d91125f0c1e26
Bruno Oliveira <83931779+bholiveiradev@users.noreply.github.com>
2024-07-11 19:52:29 -0300

    test: should return a list of payments test

--- tests/Feature/API/PaymentControllerTest.php
+++ tests/Feature/API/PaymentControllerTest.php
@@ -1,15 +1,20 @@
 <?php
 
-use App\Models\User;
+use App\Models\{Payment, User};
 
 it('should return the list of payments', function () {
     $user = User::factory()->create();
 
-    $user->client()->create([
+    $client = $user->client()->create([
         'cpf_cnpj' => '56442819060',
         'phone' => '1199999999',
         'mobile_phone' => '11999999999',
         'email' => 'test@example.com',
+    ]);
+
+    Payment::factory(10)->create([
+        'client_id' => $client->id,
+        'gateway_name' => 'testing'
     ]);
 
     $this->actingAs($user);
