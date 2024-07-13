43e7cedc7d3812b579a5627f6500dd0e73ab6cc9
Bruno Oliveira <83931779+bholiveiradev@users.noreply.github.com>
2024-07-13 02:53:40 -0300

    test: return the payment by id test

--- tests/Feature/API/PaymentControllerTest.php
+++ tests/Feature/API/PaymentControllerTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\{Payment, User};
+use Illuminate\Http\Response;
 
 it('should return the paginated list of payments', function () {
     $user = User::factory()->create();
@@ -27,4 +28,25 @@ it('should return the paginated list of payments', function () {
             'links',
             'meta',
         ]);
+});
+
+it('shoud return the payment by id', function () {
+    $user = User::factory()->create();
+
+    $client = $user->client()->create([
+        'cpf_cnpj'      => fake()->cpf(), // format: 059.949.230-95 or 05994923095
+        'phone'         => fake()->landlineNumber(), // format: (11) 9999-9999
+        'mobile_phone'  => fake()->cellphoneNumber(), // format: (11) 99999-9999
+        'email'         => $user->email,
+    ]);
+
+    $payment = Payment::factory()->create([
+        'client_id' => $client->id
+    ]);
+
+    $this->actingAs($user);
+
+    $response = $this->get('/api/payments/' . $payment->id);
+
+    expect($response)->assertStatus(Response::HTTP_OK);
 });
